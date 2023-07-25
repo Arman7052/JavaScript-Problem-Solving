4d783689f6f75c22fdd7d150413d435825c739b7
Arman7052 <99arman7052@gmail.com>
2023-07-25 21:21:48 +0600

    Task-3 Complete

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ console.log(reversed);
 
 // Example Input: [2, -5, 10, -3, 7] Example Output: 19
 
-function positiveNumSum(numbers){
+function positiveNumSum(numbers) {
   let sum = 0;
-  for(let i=0 ; i<numbers.length; i++){
-    if(numbers[i]>0){
-      sum +=numbers[i];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+      sum += numbers[i];
     }
   }
   return sum;
 }
 
-const numbers = [2, -5, 10, -3, 7] ;
+const numbers = [2, -5, 10, -3, 7];
 const total = positiveNumSum(numbers)
 console.log(total);
 
@@ -31,6 +31,35 @@ console.log(total);
 // Task 3: Write a JavaScript program to find the most frequent element in an array and return it.
 
 // Example Input: [3, 5, 2, 5, 3, 3, 1, 4, 5] Example Output: 3
+
+function findMostFrequentElement(arr) {
+  let frequentElement = null;
+  let maximumElement = 0;
+
+  for (const element of arr) {
+
+    let currentElements = 0;
+
+    for (const num of arr) {
+      if (num === element) {
+        currentElements++;
+      }
+    }
+
+    if (currentElements > maximumElement) {
+      frequentElement = element;
+      maximumElement = currentElements;
+    }
+  }
+
+  return frequentElement;
+}
+
+const elements = [3, 5, 4, 4, 4, 5, 2, 5, 3, 3, 1, 4, 4, 5];
+const result = findMostFrequentElement(elements);
+console.log(result);
+
+
 
 
 // Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
