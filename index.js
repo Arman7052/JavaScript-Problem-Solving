72f8083693b597b9a2b6f56f4c912555b1c2be06
Arman7052 <99arman7052@gmail.com>
2023-07-26 20:35:34 +0600

    Task-4 complete

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ console.log(reversed);
 
 // Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
 
-// Example Input: [2, -5, 10, -3, 7] Example Output: 19
 
 function positiveNumSum(numbers) {
   let sum = 0;
@@ -30,7 +29,6 @@ console.log(total);
 
 // Task 3: Write a JavaScript program to find the most frequent element in an array and return it.
 
-// Example Input: [3, 5, 2, 5, 3, 3, 1, 4, 5] Example Output: 3
 
 function findMostFrequentElement(arr) {
   let frequentElement = null;
@@ -64,7 +62,31 @@ console.log(result);
 
 // Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
 
-//  Example Input: ([1, 3, 6, 8, 11, 15], 9) Example Output: [1, 2] (numbers at indices 1 and 2: 3 + 6 = 9)
+function twoNumbersWithSum(arr, target) {
+  let num1 = 0;
+  let num2 = arr.length - 1;
+
+  while (num1 < num2) {
+    const sum = arr[num1] + arr[num2];
+
+    if (sum === target) {
+      return [num1, num2];
+    } else if (sum < target) {
+      num1++;
+    } else {
+      num2--;
+    }
+  }
+
+  return []; 
+}
+const targetValue = 11;
+
+const input = [1, 2, 3, 4, 6, 8, 11, 15];
+
+const indices = twoNumbersWithSum(input, targetValue);
+console.log(indices);
+
 
 
 // Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
